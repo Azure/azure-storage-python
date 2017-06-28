a70e56dd40689e3ab9044c67f156f11cc341b593
zezha-msft <zezha@microsoft.com>
2017-06-28 15:18:17 -0700

    Fail gracefully when last_sync_time on service_stats is empty

--- tests/test_service_stats.py
+++ tests/test_service_stats.py
@@ -1,4 +1,4 @@
-﻿#-------------------------------------------------------------------------
+﻿# -------------------------------------------------------------------------
 # Copyright (c) Microsoft.  All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#--------------------------------------------------------------------------
+# --------------------------------------------------------------------------
 import unittest
 
 from azure.storage.blob import BlockBlobService
@@ -21,12 +21,18 @@ from tests.testcase import (
     StorageTestCase,
     record,
 )
+from azure.storage.retry import (
+    LinearRetry
+)
 
-#------------------------------------------------------------------------------
+SERVICE_UNAVAILABLE_RESP_BODY = '<?xml version="1.0" encoding="utf-8"?><StorageServiceStats><GeoReplication><Status' \
+                              '>unavailable</Status><LastSyncTime></LastSyncTime></GeoReplication' \
+                              '></StorageServiceStats> '
 
+
+# --Test Class -----------------------------------------------------------------
 class ServiceStatsTest(StorageTestCase):
-
-    #--Helpers-----------------------------------------------------------------
+    # --Helpers-----------------------------------------------------------------
     def _assert_stats_default(self, stats):
         self.assertIsNotNone(stats)
         self.assertIsNotNone(stats.geo_replication)
@@ -34,7 +40,18 @@ class ServiceStatsTest(StorageTestCase):
         self.assertEqual(stats.geo_replication.status, 'live')
         self.assertIsNotNone(stats.geo_replication.last_sync_time)
 
-    #--Test cases per service ---------------------------------------
+    def _assert_stats_unavailable(self, stats):
+        self.assertIsNotNone(stats)
+        self.assertIsNotNone(stats.geo_replication)
+
+        self.assertEqual(stats.geo_replication.status, 'unavailable')
+        self.assertIsNone(stats.geo_replication.last_sync_time)
+
+    @staticmethod
+    def override_response_body_with_unavailable_status(response):
+        response.body = SERVICE_UNAVAILABLE_RESP_BODY
+
+    # --Test cases per service ---------------------------------------
 
     @record
     def test_blob_service_stats(self):
@@ -48,6 +65,19 @@ class ServiceStatsTest(StorageTestCase):
         self._assert_stats_default(stats)
 
     @record
+    def test_blob_service_stats_when_unavailable(self):
+        # Arrange
+        bs = self._create_storage_service(BlockBlobService, self.settings)
+        bs.response_callback = self.override_response_body_with_unavailable_status
+        bs.retry = LinearRetry(backoff=1).retry
+
+        # Act
+        stats = bs.get_blob_service_stats()
+
+        # Assert
+        self._assert_stats_unavailable(stats)
+
+    @record
     def test_queue_service_stats(self):
         # Arrange
         qs = self._create_storage_service(QueueService, self.settings)
@@ -57,6 +87,19 @@ class ServiceStatsTest(StorageTestCase):
 
         # Assert
         self._assert_stats_default(stats)
+
+    @record
+    def test_queue_service_stats_when_unavailable(self):
+        # Arrange
+        qs = self._create_storage_service(QueueService, self.settings)
+        qs.response_callback = self.override_response_body_with_unavailable_status
+        qs.retry = LinearRetry(backoff=1).retry
+
+        # Act
+        stats = qs.get_queue_service_stats()
+
+        # Assert
+        self._assert_stats_unavailable(stats)
 
     @record
     def test_table_service_stats(self):
@@ -69,6 +112,19 @@ class ServiceStatsTest(StorageTestCase):
         # Assert
         self._assert_stats_default(stats)
 
-#------------------------------------------------------------------------------
+    @record
+    def test_table_service_stats_when_unavailable(self):
+        # Arrange
+        ts = self._create_storage_service(TableService, self.settings)
+        ts.response_callback = self.override_response_body_with_unavailable_status
+        ts.retry = LinearRetry(backoff=1).retry
+
+        # Act
+        stats = ts.get_table_service_stats()
+
+        # Assert
+        self._assert_stats_unavailable(stats)
+
+# ------------------------------------------------------------------------------
 if __name__ == '__main__':
     unittest.main()
