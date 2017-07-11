e0374591f89aa7a29ffbe89af973915fc49a2c5a
Josh Friedman <jofriedm@microsoft.com>
2017-07-11 12:43:55 -0700

    Premium Page Blob Tiers

--- tests/settings_fake.py
+++ tests/settings_fake.py
@@ -20,9 +20,12 @@ STORAGE_ACCOUNT_NAME = "storagename"
 STORAGE_ACCOUNT_KEY = "NzhL3hKZbJBuJ2484dPTR+xF30kYaWSSCbs2BzLgVVI1woqeST/1IgqaLm6QAOTxtGvxctSNbIR/1hW8yH+bJg=="
 REMOTE_STORAGE_ACCOUNT_NAME = "remotestoragename"
 REMOTE_STORAGE_ACCOUNT_KEY = "3pJwX7wjxoQEW2nKAhJZARpQWpKvPOUN9JwoV/HhlMmJlS1pORhzzHpPfQqgFcwGsriu6dwYqnugWOjGShC5VQ=="
+PREMIUM_STORAGE_ACCOUNT_NAME = "premiumstoragename"
+PREMIUM_STORAGE_ACCOUNT_KEY = "NzhL3hKZbJBuJ2484dPTR+xF30kYaWSSCbs2BzLgVVI1woqeST/1IgqaLm6QAOTxtGvxctSNbIR/1hW8yH+bJg=="
 
 # Use instead of STORAGE_ACCOUNT_NAME and STORAGE_ACCOUNT_KEY if custom settings are needed
 CONNECTION_STRING = ""
+PREMIUM_CONNECTION_STRING = ""
 
 # Use 'https' or 'http' protocol for sending requests, 'https' highly recommended
 PROTOCOL = "https"
