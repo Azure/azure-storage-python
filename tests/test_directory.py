2644dcd397468f2392eed9e677ee88a0469a1a5d
zezha-msft <zezha@microsoft.com>
2017-07-11 17:50:07 -0700

    Fixed several failing tests which verify create_from* methods on blob return nothing

--- tests/test_directory.py
+++ tests/test_directory.py
@@ -218,7 +218,11 @@ class StorageDirectoryTest(StorageTestCase):
         self.assertIsNotNone(props)
         self.assertIsNotNone(props.properties.etag)
         self.assertIsNotNone(props.properties.last_modified)
-        self.assertTrue(props.properties.server_encrypted)
+
+        if self.is_file_encryption_enabled():
+            self.assertTrue(props.properties.server_encrypted)
+        else:
+            self.assertFalse(props.properties.server_encrypted)
 
 #------------------------------------------------------------------------------
 if __name__ == '__main__':
