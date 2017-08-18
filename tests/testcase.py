0ff4e9031cd92f705742dcabbf1be211ed05dfe8
zezha-msft <zezha@microsoft.com>
2017-08-17 22:49:39 -0700

    Added travis configuration

--- tests/testcase.py
+++ tests/testcase.py
@@ -16,7 +16,6 @@ from __future__ import division
 from contextlib import contextmanager
 import copy
 import inspect
-import json
 import os
 import os.path
 import time
@@ -29,12 +28,17 @@ import uuid
 import unittest
 import sys
 import random
-import tests.settings_real as settings
 import tests.settings_fake as fake_settings
 
 # Configure logging to output to console
 import logging
 logging.basicConfig(format='%(asctime)s %(name)-20s %(levelname)-5s %(message)s', level=logging.INFO)
+
+try:
+    import tests.settings_real as settings
+except ImportError:
+    settings = None
+
 
 class TestMode(object):
     none = 'None'.lower() # this will be for unit test, no need for any recordings
@@ -63,7 +67,10 @@ class StorageTestCase(unittest.TestCase):
         self.settings = settings        
         self.fake_settings = fake_settings
 
-        self.test_mode = self.settings.TEST_MODE.lower() or TestMode.playback
+        if settings is None:
+            self.test_mode = TestMode.playback
+        else:
+            self.test_mode = self.settings.TEST_MODE.lower() or TestMode.playback
         
         if self.test_mode == TestMode.playback:
             self.settings = self.fake_settings
@@ -107,7 +114,7 @@ class StorageTestCase(unittest.TestCase):
             rand = random.Random(checksum)
         result = bytearray(size)
         for i in range(size):
-            result[i] = rand.randint(0, 255)
+            result[i] = int(rand.random()*255)  # random() is consistent between python 2 and 3
         return bytes(result)
 
     def get_random_text_data(self, size):
@@ -118,7 +125,7 @@ class StorageTestCase(unittest.TestCase):
         text = u''
         words = [u'hello', u'world', u'python', u'啊齄丂狛狜']
         while (len(text) < size):
-            index = rand.randint(0, len(words) - 1)
+            index = int(rand.random()*(len(words) - 1))
             text = text + u' ' + words[index]
 
         return text
