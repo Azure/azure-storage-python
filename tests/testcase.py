20f0f07e43167fceba4f11b4b65dbd1eb0f8ad8f
Troy Dai <troy.dai@outlook.com>
2017-11-25 15:41:05 -0800

    Remove BOM from all source code

--- tests/testcase.py
+++ tests/testcase.py
@@ -1,4 +1,5 @@
-﻿#-------------------------------------------------------------------------
+# coding: utf-8
+#-------------------------------------------------------------------------
 # Copyright (c) Microsoft.  All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
