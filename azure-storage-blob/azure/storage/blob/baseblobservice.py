8d8e950d31b1abb7331b94a10b75bdc8e2178f79
Nicolai Søborg <nis@spiir.dk>
2020-04-01 13:37:03 +0200

    Fix "SyntaxWarning: `is` with a literal."

--- azure-storage-blob/azure/storage/blob/baseblobservice.py
+++ azure-storage-blob/azure/storage/blob/baseblobservice.py
@@ -1068,7 +1068,7 @@ class BaseBlobService(StorageClient):
         :return: str
         '''
         _validate_not_none('lease_duration', lease_duration)
-        if lease_duration is not -1 and \
+        if lease_duration != -1 and \
                 (lease_duration < 15 or lease_duration > 60):
             raise ValueError(_ERROR_INVALID_LEASE_DURATION)
 
@@ -2776,7 +2776,7 @@ class BaseBlobService(StorageClient):
         '''
         _validate_not_none('lease_duration', lease_duration)
 
-        if lease_duration is not -1 and \
+        if lease_duration != -1 and \
                 (lease_duration < 15 or lease_duration > 60):
             raise ValueError(_ERROR_INVALID_LEASE_DURATION)
         lease = self._lease_blob_impl(container_name,
